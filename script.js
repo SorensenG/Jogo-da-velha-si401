2a4f0f701315087e74153979728429164ad6f043
Gabriel Sorensen <g.soren.sen2004@gmail.com>
2025-09-23 20:04:53 -0300

    primeiras implementaçoes

---
+++ script.js
@@ -0,0 +1,10 @@
+const winConditions = [
+    [0, 1, 2], // Linha horizontal superior
+    [3, 4, 5], // Linha horizontal do meio
+    [6, 7, 8], // Linha horizontal inferior
+    [0, 3, 6], // Coluna vertical esquerda
+    [1, 4, 7], // Coluna vertical do meio
+    [2, 5, 8], // Coluna vertical direita
+    [0, 4, 8], // Diagonal principal
+    [2, 4, 6]  // Diagonal secundária
+];
